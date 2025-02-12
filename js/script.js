963e04608e16318e40d1cf792d37d0a9b0930c3e
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-02-12 06:56:15 +0200

    v

--- js/script.js
+++ js/script.js
@@ -506,30 +506,29 @@ const lastName = "Marleys";
 
 
 
-// Запитай користувача у модальному вікні, який з напоїв він бажає придбати - "Кава", "Чай" чи "Сік",
-// та залежно від виду напою оголоси суму досплати.Використовувати switch.
 
-// let drink = prompt("Що бажаєте?")?.toLowerCase();
-// // if (typeof drink === "string") {
-// //   drink = drink.toLowerCase();
-// // }
 
-// let price = 0;
-
-// switch (drink) {
-//   case "кава":
-//     price = 1;
-//     break;
-//   case "чай":
-//     price = 2;
-//     break;
-//   case "сік":
-//     price = 3;
-//     break;
-//   default:
-//     "Що це?";
+let drink = prompt("Що бажаєте?")?.toLowerCase();
+// if (typeof drink === "string") {
+//   drink = drink.toLowerCase();
 // }
-// console.log(price);
+
+let price = 0;
+
+switch (drink) {
+  case "кава":
+    price = 1;
+    break;
+  case "чай":
+    price = 2;
+    break;
+  case "сік":
+    price = 3;
+    break;
+  default:
+    "Що це?";
+}
+console.log(price);
 
 /*
 Опис:
@@ -635,66 +634,37 @@ console.log(sum);
 TODO Тіммі та Сара думають, що вони закохані, але в тому місці, де вони живуть, вони дізнаються лише тоді, коли зірвуть по одній квітці. Якщо одна з квіток має парну кількість пелюсток, а інша - непарну, то це означає, що вони закохані. Напишіть функцію, яка отримує кількість пелюсток кожної квітки і повертає true, якщо вони закохані, і false, якщо ні.
 TODO – Var 1
 function checkLove(flower1, flower2) {
-  // if (
-  //   (flower1 % 2 === 0 && flower2 % 2 !== 0) ||
-  //   (flower1 % 2 !== 0 && flower2 % 2 === 0)
-  // ) {
-  //   return true;
-  // }
-  // return false;
   return flower1 % 2 !== flower2 % 2;
 }
-
 console.log(checkLove(2, 3));
+TODO – Var 2
+function checkLove(flower1, flower2) {
+  if (
+    (flower1 % 2 === 0 && flower2 % 2 !== 0) ||
+    (flower1 % 2 !== 0 && flower2 % 2 === 0)
+  ) {
+    return true;
+  }
+  return false;
+}
+console.log(checkLove(3, 3));
 */
-
-function checkLove(flower1, flower2) {
-  // if (
-  //   (flower1 % 2 === 0 && flower2 % 2 !== 0) ||
-  //   (flower1 % 2 !== 0 && flower2 % 2 === 0)
-  // ) {
-  //   return true;
-  // }
-  // return false;
-  return flower1 % 2 !== flower2 % 2;
+/* //! Question 5
+TODO Запитай користувача у модальному вікні, який з напоїв він бажає придбати - "Кава", "Чай" чи "Сік",та залежно від виду напою оголоси суму досплати. Використовувати switch.
+let drink = prompt('Що бажаєте?')?.toLowerCase();
+let price = 0;
+switch (drink) {
+  case 'кава':
+    price = 1;
+    break;
+  case 'чай':
+    price = 2;
+    break;
+  case 'сік':
+    price = 3;
+    break;
+  default:
+    'Що це?';
 }
-
-console.log(checkLove(2, 3));
-
-function checkLove(flower1, flower2) {
-  // if (
-  //   (flower1 % 2 === 0 && flower2 % 2 !== 0) ||
-  //   (flower1 % 2 !== 0 && flower2 % 2 === 0)
-  // ) {
-  //   return true;
-  // }
-  // return false;
-  return flower1 % 2 !== flower2 % 2;
-}
-
-console.log(checkLove(2, 3));
-
-// ? Є рядок 'HTML, CSS, JS, PHP, React, Vue, Node.js, C, C++'. Дізнайтеся, чи є в цьому рядку мова: 'JavaScript', або 'JS'?
-// ? Якщо є, дізнайтесь позицію на якій перебуває цей підрядок
-// const languages = "HTML, CSS, JS, PHP, React, Vue, Node.js, C, C++";
-// function fineSubString(string, subString) {
-//   if (string.includes(subString)) {
-//     return string.indexOf(subString);
-//   }
-//   return "Not found";
-// }
-
-// console.log(fineSubString(languages, "JS"));
-
-//? У нас є рядок '24px', дізнайтеся на що закінчується цей рядок, на: %, rem, em або px;
-// const str = "24px";
-
-// const result = str.startsWith("2");
-// console.log(result);
-
-// ?  У нас є рядок: 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?'.
-// ? Замініть усі входження dog на monkey.
-// const str =
-//   "The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?";
-// console.log(str.replace("The", "On"));
-// console.log(str.replaceAll("dog", "monkey"));
+console.log(price);
+*/
