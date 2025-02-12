df6251df3ba29fe6d13122fd987d4623ea2b95a6
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-02-12 14:59:15 +0200

    v

---
+++ js/script.js
@@ -0,0 +1,38 @@
+/*
+TODO – Var 1
+const languagesStr = 'HTML, CSS, JS, JS, PHP, React, Vue, Node.js, C, C++';
+
+let uniqueLetters = '';
+
+// for (let i = 0; i < languagesStr.length; i += 1) {
+//   if (uniqueLetters.includes(languagesStr[i])) {
+//     continue;
+//   }
+//   uniqueLetters += languagesStr[i];
+// }
+// console.log(uniqueLetters);
+TODO – Var 2
+const languagesStr = 'HTML, CSS, JS, JS, PHP, React, Vue, Node.js, C, C++';
+
+let uniqueLetters = '';
+for (let i = 0; i < languagesStr.length; i += 1) {
+  if (!uniqueLetters.includes(languagesStr[i])) {
+    uniqueLetters += languagesStr[i];
+  }
+}
+
+console.log(uniqueLetters);
+
+TODO – Var 3
+const languagesStr = 'HTML, CSS, JS, JS, PHP, React, Vue, Node.js, C, C++';
+
+let uniqueLetters = '';
+
+for (let i = 0; i < languagesStr.length; i += 1) {
+  if (i === languagesStr.indexOf(languagesStr[i])) {
+    uniqueLetters += languagesStr[i];
+  }
+}
+
+console.log(uniqueLetters);
+*/
