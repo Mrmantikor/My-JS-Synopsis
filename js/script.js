8717b0834eb2f48ad716d33f03574ab8206fc061
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-02-12 07:05:47 +0200

    v

--- js/script.js
+++ js/script.js
@@ -496,44 +496,6 @@ const lastName = "Marleys";
 
 // console.log(checkAgeMaksim(17));
  */
-/* //! Lesson 2-2 Practical
-
-
-
-
-
-
-
-
-
-
-
-let drink = prompt("Що бажаєте?")?.toLowerCase();
-// if (typeof drink === "string") {
-//   drink = drink.toLowerCase();
-// }
-
-let price = 0;
-
-switch (drink) {
-  case "кава":
-    price = 1;
-    break;
-  case "чай":
-    price = 2;
-    break;
-  case "сік":
-    price = 3;
-    break;
-  default:
-    "Що це?";
-}
-console.log(price);
-
-/*
-Опис:
-
-*/
 
 //! Math.pow(2, 3); // Степінь
 //! console.log(Math.sqrt(8)); // Повертає квадратний корінь числа
@@ -668,3 +630,28 @@ switch (drink) {
 }
 console.log(price);
 */
+/* //! Question 6 
+TODO Є рядок 'HTML, CSS, JS, PHP, React, Vue, Node.js, C, C++'. Дізнайтеся, чи є в цьому рядку мова: 'JavaScript', або 'JS'? Якщо є, дізнайтесь позицію на якій перебуває цей підрядок.
+const languages = 'HTML, CSS, JS, PHP, React, Vue, Node.js, C, C++';
+function fineSubString(string, subString) {
+  if (string.includes(subString)) {
+    return string.indexOf(subString);
+  }
+  return 'Not found';
+}
+console.log(fineSubString(languages, 'JS'));
+*/
+/* //! Question 7
+TODO У нас є рядок '24px', дізнайтеся на що закінчується цей рядок, на: %, rem, em або px;
+const str = '24px';
+const result = str.startsWith('2');
+console.log(result);
+ */
+/* //! Question 8
+TODO У нас є рядок: 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?'. Замініть усі входження dog на monkey.
+const str = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?';
+console.log(str.replace('The', 'On'));
+console.log(str.replaceAll('dog', 'monkey'));
+ */
+/* //! Question 9
+ */
