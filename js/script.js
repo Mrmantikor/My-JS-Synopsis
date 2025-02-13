f3a07cdb0a5a361fc1d3259ba5ab06fa26788726
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-02-13 19:29:58 +0200

    v0

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 console.time('for...of');
 console.timeEnd('for...of');
 */
+/* //! if else
+const lang = prompt('How your love languages?');
+if (lang === 'javascript') {
+  console.log('Yes of course!!!');
+} else {
+  console.log('Are you sure?!');
+}
+*/
+
