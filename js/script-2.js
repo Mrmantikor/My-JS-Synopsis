0e290ae9f4ec776b202b24e2f3417a769b2e9c39
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-02-13 16:36:53 +0200

    v

--- js/script-2.js
+++ js/script-2.js
@@ -467,7 +467,132 @@ for (const item of friends) {
   console.log(item);
 }
 */
+/* //! Task #4 split ()
+TODO: Напиши скрипт для обчислення площі прямокутника зі сторонами, значення яких зберігаються у змінній values у вигляді рядка. Значення гарантовано розділені пробілом.
+const values = '8 11';
+const sidesArr = values.split(' ');
+console.log('values:', values);
+console.log('sidesArr:', sidesArr);
+const rectangleArea = Number(sidesArr[0]) * Number(sidesArr[1]);
+console.log('rectangleArea:', rectangleArea);
+*/
+/* //! Task #5 for(of)
+TODO: Напиши скрипт, який підраховує суму всіх парних чисел у масиві.
+const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
+let total = 0;
+for (const num of numbers) {
+  if (num % 2 === 0) {
+    total += num;
+  }
+}
+console.log('numbers:', numbers);
+console.log('total:', total);
+ */
+/* //! Task #6 for()
+TODO: Напиши скрипт, який виводить у консоль ім'я та телефонний номер користувача. У змінних names та phones зберігаються рядки імен та телефонних номерів, розділені комами. Порядковий номер імен та телефонів у рядках вказують на відповідність. Кількість імен та телефонів гарантовано однакова.
+const names = 'Jacob,William,Solomon,Artemis';
+const phones = '89001234567,89001112233,890055566377,890055566300';
+console.log('names:', names);
+console.log('phones:', phones);
+const namesArr = names.split(',');
+const phonesArr = phones.split(',');
+console.log('namesArr:', namesArr);
+console.log('phonesArr:', phonesArr);
+for (let i = 0; i < namesArr.length; i++) {
+  console.log(`${namesArr[i]} - ${phonesArr[i]}`);
+}
+*/
+/* //! Task #7 split() join() indexOf() for()
+TODO: Напиши скрипт, який виводить у консоль усі слова рядка крім першого і останнього. Результуючий рядок не повинен починатися або закінчуватися символ пробілу. Скрипт повинен працювати для будь-якого рядка.
+TODO – Var 1
+const string = 'Welcome to the future';
+const wordsArr = string.split(' ');
+let resultStr = '';
+console.log('string:', string);
+console.log('wordsArr:', wordsArr);
+for (let i = 1; i < wordsArr.length - 1; i++) {
+  resultStr += ` ${wordsArr[i]}`;
+}
+resultStr = resultStr.trim();
+console.log('resultStr:', resultStr);
+TODO – Var 2
+const string = 'Welcome to the future';
+let wordsArr = string.split(' ');
+wordsArr = wordsArr.slice(1, wordsArr.length - 1);
+const resultStr = wordsArr.join(' ');
+console.log('string:', string);
+console.log('wordsArr:', wordsArr);
+console.log('resultStr:', resultStr);
+TODO – Var 3
+const string = 'Welcome to the future';
+const firstSpace = string.indexOf(' ');
+const lastSpace = string.lastIndexOf(' ');
+console.log('string:', string);
+console.log('firstSpace:', firstSpace);
+console.log('lastSpace:', lastSpace);
+const resultStr = string.slice(firstSpace + 1, lastSpace);
+console.log('resultStr:', resultStr);
+*/
+/* //! Task #8 for()
+TODO: Напиши скрипт, який «розгортає» рядок (зворотній порядок літер) та виводить її в консоль.
+TODO – Var 1
+const string = 'Welcome to the future';
+const lettersArr = string.split('');
+let reversedStr = '';
+console.log('string:', string);
+console.log('lettersArr:', lettersArr);
+for (let i = lettersArr.length - 1; i >= 0; i--) {
+  reversedStr += lettersArr[i];
+}
+console.log('reversedStr:', reversedStr);
 
+TODO – Var 2
+const string = 'Welcome to the future';
+let reversedStr = '';
+for (let i = string.length - 1; i >= 0; i--) {
+  reversedStr += string[i];
+}
+console.log('string:', string);
+console.log('reversedStr:', reversedStr);
+*/
+/* //! Task #9 for()
+TODO: Напиши скрипт пошуку найменшого числа у масиві. Код повинен працювати для будь-якого масиву чисел. Використовуй цикл для розв'язання задачі.
+const values = [17, -10, 94, -30, 1, 23, -20];
+let max = values[0];
+for (let i = 1; i < values.length; i++) {
+  if (values[i] > max) {
+    max = values[i];
+  }
+}
+console.log('values:', values);
+console.log('max:', max);
+*/
+/* //! Task #10 for()
+TODO: У нас є декілько масивів із зарплатами працівників із різних відділів, потрібно порахувати загальну суму зарплат
+const managerSalaries = [100, 150, 250, 400, 500];
+const developersSalaries = [800, 1500, 4000];
+let total = 0;
+for (const salary of managerSalaries) {
+  total += salary;
+}
+console.log('total:', total);
+for (const salary of developersSalaries) {
+  total += salary;
+}
+console.log('total:', total);
+*/
+/* //! Task #11
+ */
+/* //! Task #12
+ */
+/* //! Task #13
+ */
+/* //! Task #14
+ */
+/* //! Task #15
+ */
+/* //! Task #16
+ */
 // #endregion
 /* //! localeCompare()
 TODO: Правильне порівняння строк
