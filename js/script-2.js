bfe116823a990f2027168955d04d1ca7c201c086
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-02-13 16:43:20 +0200

    v

--- js/script-2.js
+++ js/script-2.js
@@ -581,9 +581,40 @@ for (const salary of developersSalaries) {
 }
 console.log('total:', total);
 */
-/* //! Task #11
+/* //! Task #11 push()
+TODO: Створіть масив styles з елементами «Джаз» та «Блюз». Додайте "Рок-н-рол" в кінець. Замініть значення "Блюз" на "Класика". Видалить перший елемент масиву і виведіть його в консоль. Вставте «Реп» і «Реггі» на початок масиву.
+const styles = ['Джас', 'Блюз'];
+styles.push('Рок-н-рол');
+const index = styles.indexOf('Блюз');
+styles[index] = 'Класика';
+const deleteElement = styles.shift();
+styles.unshift('«Реп»', 'Реггі');
+console.log(styles);
+
  */
 /* //! Task #12
+TODO: Створити функцію multiples(m, n), яка повертає масив перших m кратних дійсного числа n. Вважати, що m - натуральне число.
+TODO: Наприклад. multiples(3, 5) повинна повернути [5, 10, 15]
+TODO – Var 1
+function multiples(m, n) {
+  const arr = [];
+  let startNum = n;
+  for (let i = 0; i < m; i += 1) {
+    arr.push(startNum);
+    startNum += n;
+  }
+  return arr;
+}
+console.log(multiples(4, 4));
+TODO – Var 2
+function multiples(m, n) {
+  const arr = [];
+  for (let i = 1; i <= m; i += 1) {
+    arr.push(n * i);
+  }
+  return arr;
+}
+console.log(multiples(4, 4));
  */
 /* //! Task #13
  */
@@ -593,60 +624,6 @@ console.log('total:', total);
  */
 /* //! Task #16
  */
-
-// Створіть масив styles з елементами «Джаз» та «Блюз».
-// Додайте "Рок-н-рол" в кінець.
-// Замініть значення "Блюз" на "Класика".
-// Видалить перший елемент масиву і виведіть його в консоль.
-// Вставте «Реп» і «Реггі» на початок масиву.
-
-// const styles = ["Джас", "Блюз"];
-
-// styles.push("Рок-н-рол");
-
-// const index = styles.indexOf("Блюз");
-
-// // styles[index] = "Класика";
-
-// const deleteElement = styles.shift();
-
-// styles.unshift("«Реп»", "Реггі");
-
-// console.log(styles);
-
-/*
-Створити функцію multiples(m, n), яка повертає масив перших m кратних дійсного числа n. Вважати, що m - натуральне число.
-
-Наприклад.
-multiples(3, 5) повинна повернути [5, 10, 15]
-*/
-
-// function multiples(m, n) {
-//   const arr = [];
-//   let startNum = n;
-
-//   for (let i = 0; i < m; i += 1) {
-//     arr.push(startNum);
-
-//     startNum += n;
-//   }
-
-//   return arr;
-// }
-
-// option 2
-// function multiples(m, n) {
-//   const arr = [];
-
-//   for (let i = 1; i <= m; i += 1) {
-//     arr.push(n * i);
-//   }
-
-//   return arr;
-// }
-
-// // multiples(3, 5);
-// console.log(multiples(4, 4));
 
 //Перетворити рядок, що містить слова, розділені комами, в масив слів і вивести кожне слово в окремому рядку за допомогою циклу for і for..of.
 
