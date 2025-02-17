3e4b41780a4decbbad37b781c5925b4cffc8cd92
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-02-17 19:22:10 +0200

    obj

--- js/script.js
+++ js/script.js
@@ -39,15 +39,56 @@ if (size === 'small') {
 
 
 /*
-dot notation
-brackets notation
-compound brackets notation
+
+
+
 */
 
 // #region //?_Об'єкти_Теоргія?//
+/* //! dot notation  
+human.mood = "happy";
+
+ const human = {
+     name: "Mango",
+     hobby: "reading",
+     age: 20,
+     greet(){
+         console.log(`Hello ${human.name}`);
+     }
+};
+human.greet()
+*/
+/* //! brackets notation
+human["mood"] = "happy";
+
+ const human = {
+     name: "Mango",
+     hobby: "reading",
+     age: 20,
+     greet(){
+         console.log(`Hello ${human.name}`);
+     }
+};
+human.greet()
+*/
+/* //! compound brackets notation
+const userMood = "mood";
+human[userMood] = "happy";
+
+ const human = {
+     name: "Mango",
+     hobby: "reading",
+     age: 20,
+     greet(){
+         console.log(`Hello ${human.name}`);
+     }
+};
+human.greet()
+ console.log(human);
+*/
+
 /* //! for...in
 TODO: Порахуйте загальну кількість відгуків.
-TODO – Var 1 (Через цикл for...in)
 const feedbacks = {
   good: 5,
   neutral: 10,
@@ -65,7 +106,7 @@ for (const key in feedbacks) {
 console.log('totalFeedbacks:', totalFeedbacks);
 */
 /* //! Object.keys()
-TODO – Var 2 (Через метод Object.keys() )
+TODO: Порахуйте загальну кількість відгуків.
 const feedbacks = {
   good: 5,
   neutral: 10,
@@ -83,7 +124,7 @@ for (const key of feedbacksKeys) {
 console.log('totalFeedbacks:', totalFeedbacks);
 */
 /* //! Object.values()
-TODO – Var 3 (Через метод Object.values() )
+TODO: Порахуйте загальну кількість відгуків.
 const feedbacks = {
   good: 5,
   neutral: 10,
@@ -102,65 +143,33 @@ console.log(totalFeedbacks);
 */
 // #endregion
 // #region //?_Об'єкти_Практика?//
-
-
-//#endregion
- const human = {
-     name: "Mango",
-     hobby: "reading",
-     age: 20,
-     greet(){
-         console.log(`Hello ${this.name}`);
-     }
-};
- 
-human.greet()
-human.mood = "happy";
-human["mood"] = "happy";
-
- console.log(human);
 /*
+TODO: Потрібно написати функцію, яка приймає 2 параметри obj і key, яка буде перебирати об'єкт. Якщо об'єкт має такий ключ - поверне true
+const obj = {
+    name: 'Igor',
+    car: 'Mercedes',
+    carColor: 'black',
+  };
 
-
- const userMood = "mood";
-human[userMood] = "happy";
- human.hobby = "skydiving";
- human["hobby"] = "skydiving";
- const userHobby = "hobby";
- human[userHobby]= "skydiving";
+ const checkObj = function (obj, key){
+     const keys = Object.keys(obj);
+     for (const item of keys){
+         if (item === key){
+             return true;
+         }
+     }
+     return false;
+}
+ 
+console.log(checkObj(obj, "car"));
 */
+//#endregion
 
-// // Створіть об'єкт "людина" з властивостями "ім'я", "хобі" і "вік". Напишіть метод "greet", який буде виводити на екран привітання з ім'ям людини.
-// // Послідовно:
-// // 1 додати поле mood зі значенням 'happy'
-// // 2 замінити hobby на 'skydiving'
-//
 
-//
-// // 
-//
 
-//
-// // console.log(human);
-//
-// // Потрібно написати функцію, яка приймає 2 параметри obj і key, яка буде перебирати об'єкт.
-// // Якщо об'єкт має такий ключ - поверне true
-//
-// const obj = {
-//     name: 'Igor',
-//     car: 'Mercedes',
-//     carColor: 'black',
-//   };
-//
-// // const checkObj = function (obj, key){
-// //     const keys = Object.keys(obj);
-// //     for (const item of keys){
-// //         if (item === key){
-// //             return true;
-// //         }
-// //     }
-// //     return false;
-// // }
+
+
+
 //
 // // const checkObj = function (obj, key){
 // //     return Object.keys(obj).includes(key);
