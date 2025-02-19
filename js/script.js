2d8d1c9348ecf6e06f2675f6b69c298d72dcce5d
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-02-19 13:57:17 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 console.time('for...of');
 console.timeEnd('for...of');
 */
+
+function repeatStr(n, s) {
+  return;
+}
+
+console.log((repeatStr(3, '*'), '***'));
+
+const methodRepeat = 'You NOT Love is Java Skript?!';
+console.log(methodRepeat.repeat(3));
