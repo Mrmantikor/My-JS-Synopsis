0f0502fdfe1f6b3c2bec820f8353c5c24011d087
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-02-21 18:09:25 +0200

    Update script-2.js

--- js/script-2.js
+++ js/script-2.js
@@ -1355,7 +1355,32 @@ const obj = {
  }
 console.log(checkObj(obj, "car"));
 */
+const allCars = [
+  { make: 'Honda', model: 'Accord', amount: 2, price: 22455 },
+  { make: 'Mazda', model: 'Mazda 6', amount: 8, price: 24195 },
+  { make: 'Mazda', model: 'CX-9', amount: 7, price: 31520 },
+  { make: 'Toyota', model: '4Runner', amount: 19, price: 34210 },
+  { make: 'Toyota', model: 'Sequoia', amount: 16, price: 45560 },
+  { make: 'Toyota', model: 'Tacoma', amount: 4, price: 24320 },
+  { make: 'Ford', model: 'F-150', amount: 11, price: 27110 },
+  { make: 'Ford', model: 'Fusion', amount: 13, price: 22120 },
+  { make: 'Ford', model: 'Explorer', amount: 6, price: 31660 },
+  { make: 'Honda', model: 'CR-V', amount: 14, price: 24045 },
+];
+const makeCarsWithDiscount = (cars, discount) => {
+  cars = structuredClone(cars);
 
+  return cars.map(car => {
+    car.price = car.price - car.price * discount;
+
+    return car;
+  });
+};
+
+console.table(makeCarsWithDiscount(allCars, 0.2));
+console.table(makeCarsWithDiscount(allCars, 0.4));
+
+console.table(allCars);
 /* //! масив.forEach(el, idx, arr)
 TODO: масив.forEach(function(element, index, array) => {ТУТ код});
  * - Поелементно перебирає оригінальний масив
