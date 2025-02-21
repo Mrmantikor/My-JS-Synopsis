5c4171c344afbcbdabfede7eb1794d7a0dd3a95c
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-02-21 17:28:07 +0200

    arr

--- js/script-2.js
+++ js/script-2.js
@@ -1356,72 +1356,34 @@ const obj = {
 console.log(checkObj(obj, "car"));
 */
 
-/* //! .prototype.map()
-TODO:
- * - Поелементо перебирає оригінальний масив
- * - Не змінює оригінальний масив
- * - Повертає новий масив такої ж довжини
-
-
- TODO: Створіть новий масив, в якому усі елементи масиву numbers будуть помножені на 2.
-
-~ Через метод forEach()
+/* //! масив.forEach(el, idx, arr)
+TODO: масив.forEach(function(element, index, array) => {ТУТ код});
+ * - Поелементно перебирає оригінальний масив
+ * - Не змінює оригінальний масив (але може змінювати його елементи, якщо вони є об'єктами)
+ * - Не повертає новий масив, а просто виконує передану функцію для кожного елемента масиву
+ * - На відміну від map(), метод forEach() не створює і не повертає новий масив, а лише виконує функцію для кожного елемента.
+TODO: Створіть новий масив, в якому усі елементи масиву numbers будуть помножені на 2.
 const numbers = [1, 2, 3, 4, 5];
 const doubleNumbers = [];
-
 numbers.forEach(el => doubleNumbers.push(el * 2));
 
 console.log('numbers:', numbers);
 console.log('doubleNumbers:', doubleNumbers);
-
-~ Через метод map()
+*/
+/* //! масив.map(el, idx, arr)
+TODO: масив.map((element, index, array) => {ТУТ код});
+ * - Поелементо перебирає оригінальний масив
+ * - Не змінює оригінальний масив
+ * - Повертає новий масив такої ж довжини
+TODO: Створіть новий масив, в якому усі елементи масиву numbers будуть помножені на 2.
 const numbers = [1, 2, 3, 4, 5];
 const doubleNumbers = numbers.map(el => el * 2);
 
 console.log('numbers:', numbers);
 console.log('doubleNumbers:', doubleNumbers);
 
-
 TODO: Нехай функція getModels повертає масив моделей (поле model) всіх автомобілів.
- */
-
-// const allCars = [
-//   { make: 'Honda', model: 'CR-V', amount: 14, price: 24045 },
-//   { make: 'Honda', model: 'Accord', amount: 2, price: 22455 },
-//   { make: 'Mazda', model: 'Mazda 6', amount: 8, price: 24195 },
-//   { make: 'Mazda', model: 'CX-9', amount: 7, price: 31520 },
-//   { make: 'Toyota', model: '4Runner', amount: 19, price: 34210 },
-//   { make: 'Toyota', model: 'Sequoia', amount: 16, price: 45560 },
-//   { make: 'Toyota', model: 'Tacoma', amount: 4, price: 24320 },
-//   { make: 'Ford', model: 'F-150', amount: 11, price: 27110 },
-//   { make: 'Ford', model: 'Fusion', amount: 13, price: 22120 },
-//   { make: 'Ford', model: 'Explorer', amount: 6, price: 31660 },
-// ];
-
-//~ Повна версія
-// const getModels = cars => {
-//   const allModels = cars.map((el, idx, arr) => {
-//     return el.model;
-//   });
-
-//   return allModels;
-// };
-
-// console.log(getModels(allCars));
-
-//~ Скорочена версія
-// const getModels = cars => cars.map(car => car.model);
-
-// console.log(getModels(allCars));
-/*
- * Array.prototype.map()
- * - Поелементо перебирає оригінальний масив
- * - Не змінює оригінальний масив
- * - Повертає новий масив такої ж довжини
-
-
 const allCars = [
-  { make: 'Honda', model: 'CR-V', amount: 14, price: 24045 },
   { make: 'Honda', model: 'Accord', amount: 2, price: 22455 },
   { make: 'Mazda', model: 'Mazda 6', amount: 8, price: 24195 },
   { make: 'Mazda', model: 'CX-9', amount: 7, price: 31520 },
@@ -1431,9 +1393,29 @@ const allCars = [
   { make: 'Ford', model: 'F-150', amount: 11, price: 27110 },
   { make: 'Ford', model: 'Fusion', amount: 13, price: 22120 },
   { make: 'Ford', model: 'Explorer', amount: 6, price: 31660 },
+  { make: 'Honda', model: 'CR-V', amount: 14, price: 24045 },
 ];
 
+  .//!  Повна версія
+const getModels = cars => {
+  const allModels = cars.map((el, idx, arr) => {
+    return el.model;
+  });
+  return allModels;
+};
 
+console.log(getModels(allCars));
+
+  .//!  Скорочена версія
+const getModels = cars => cars.map(car => car.model);
+
+console.log(getModels(allCars));
+*/
+/* //! const новийОб'єкт = structuredClone(оригінальнийОб'єкт);
+
+*/
+
+/*
 TODO: Нехай функція makeCarsWithDiscount повертає новий масив об'єктів із змінним
 TODO: значенням властивості price залежно від переданої знижки. Метод для копії structuredClone.
 
