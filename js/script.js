2e315d04e4887c73a427767af8b753c97e643fc8
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-02-28 14:44:56 +0200

    dom bom

---
+++ js/script.js
@@ -0,0 +1,124 @@
+//#region //! ES, DOM, BOM
+/* //! ОСНОВИ Об'єктна модель документа
+ * JavaScript in browsers
+ *
+ * - ECMAScript (ES) - це основа мови JavaScript.
+ * - DOM (Document Object Model) - Керує вмістом сторінки (елементами HTML). Це програмний інтерфейс для веб-документів. Він представляє HTML-сторінку у вигляді дерева об'єктів, де кожен елемент є окремим вузлом (Node), до якого можна звертатися та змінювати його за допомогою JavaScript.
+ * - BOM (Browser Object Model) - Керує вікном браузера і його параметрами. Це модель об'єктів браузера, яка дозволяє JavaScript взаємодіяти з вікном браузера та його елементами, що не належать до DOM
+
+.//! Навігація по DOM
+TODO: Елементи DOM-дерева мають ієрархічне відношення один до одного. Для опису відносин використовуються терміни:
+ * - предок (ancestor)
+ * - нащадок (descendant)
+ * - батько (parent)
+ * - дитина (child)
+ * - сусід (sibling)
+*/
+/* //! document.querySelector('selector') (Пошук елементів)
+TODO: document.querySelector('selector')  Метод використовується для пошуку першого елемента на сторінці, який відповідає вказаному CSS-селектору.
+ * Виконує пошук HTML елементу:
+ * - За назвою тегу
+ * - За назвою класу
+ * - За ID
+ * - Повертає ПЕРШИЙ знайдений елемент або null, якщо елемента немає.
+ * - Використовує CSS-синтаксис селекторів, як у CSS.
+ * - console.log(); -→ Якщо передати DOM-елемент, у консолі відобразиться його HTML-структура.
+ * - console.dir(); -→ Виводить повний об'єкт із його властивостями та методами у вигляді дерева.
+ * 
+TODO var - 1 //? За назвою тегу
+HTML
+<button class="clas-button js-button" id="magic-button" type="button">TEXT</button>
+
+JS
+const consoleButton = document.querySelector('button'); 
+console.log(consoleButton);
+console.dir(consoleButton);
+TODO var - 2 //? За назвою класу
+HTML
+<button class="clas-button js-button" id="magic-button" type="button">TEXT</button>
+
+JS
+const consoleButton = document.querySelector('.clas-button'); 
+console.dir(consoleButton);
+console.log(consoleButton);
+TODO var - 3 //? За ID 
+HTML
+<button class="clas-button js-button" id="magic-button" type="button">TEXT</button>
+
+JS
+const consoleButton = document.querySelector('#magic-button'); 
+console.log(consoleButton);
+console.dir(consoleButton);
+*/
+/* //! document.querySelectorAll('selector') (Пошук ВСІХ елементів)
+TODO: document.querySelectorAll('selector') Метод, який повертає всі елементи на сторінці, що відповідають вказаному CSS-селектору.
+ * Виконує пошук HTML елементу:
+ * - За назвою тегу
+ * - За назвою класу
+ * - За ID
+ * - Повертає всі знайдені елементи або пустий псевдо-масив, якщо елемента немає.
+ * - Використовує CSS-синтаксис селекторів, як у CSS.
+ * 
+HTML
+<ul class="list">
+  <li class="link"></li>
+  <li class="link"></li>
+  <li class="link"></li>
+  <li class="link"></li>
+</ul>;
+
+JS
+const linkSelector = document.querySelectorAll('.link');
+console.log(linkSelector);
+console.dir(linkSelector);
+*/
+/* //! Перезапис атрибутів
+HTML
+<ul class="list">
+  <li class="link">
+    <a href="https://www.google.com/" class="social" target="_blank">
+      social
+    </a>
+  </li>
+</ul>;
+
+JS
+const browser = document.querySelector('.social');
+console.log(browser); -→ HTML-структура
+console.log(browser.href); -→ https://www.google.com/
+browser.href = 'https://www.bing.com/'; -→ Перезаписуємо
+console.log(browser.href); -→ https://www.bing.com/
+*/
+/* //! .textContent (Змінювати текстовий вміст)
+TODO: textContent - це властивість, яка дозволяє отримувати або змінювати текстовий вміст елемента, включаючи весь текст усередині нього, навіть прихований через display: none
+HTML
+<p class="text">
+  Bohdan <span class="sub-text">Vasylovich</span>
+</p>;
+
+JS
+const elP = document.querySelector('.text');
+const elSpan = document.querySelector('.sub-text');
+console.log(elP.textContent); -→ Bohdan Vasylovich
+console.log(elSpan.textContent); -→ Vasylovich
+elSpan.textContent = 'Lukashenko'; -→ Змінюємо Span
+console.log(elP.textContent); -→ Bohdan Lukashenko
+console.log(elSpan.textContent); -→ Lukashenko
+*/
+
+//#endregion
+
+/* //! .classList 
+TODO: - властивість, яка дозволяє додавати, видаляти чи переводити класи елемента
+ * - .add('class') Додає клас
+ * - .remove('class') Видаляє клас
+ * - .toggle('class') Перемикає
+ * - .contains('class') Перевіряє, чи є клас у елемента (true або false)
+ * - .replace('old', 'new') Замінює один клас іншим
+ * 
+HTML
+<a href="https://www.google.com/" class="social" target="_blank">social</a>
+JS
+const link = document.querySelector('.social');
+console.log(link.classList);
+*/
